ad3b790a3745a3ea2e86adb44a09c1583218f4ce
ahmed-amr-12 <amra84747@gmail.com>
2026-03-04 18:18:58 +0200

    fix token from cookies in vercel

--- src/Actions/GetMyToken.ts
+++ src/Actions/GetMyToken.ts
@@ -6,8 +6,9 @@ import { cookies } from "next/headers"
 export async function GetMyToken() {
 
   const myCookies = await cookies()
-  const TokenFromCookies = myCookies.get("next-auth.session-token")?.value
-
+const TokenFromCookies =
+  myCookies.get("next-auth.session-token")?.value ||
+  myCookies.get("__Secure-next-auth.session-token")?.value;
   console.log("TokenFromCookies", TokenFromCookies);
 
   const decodedToken = await decode({
