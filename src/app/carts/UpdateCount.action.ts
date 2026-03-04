19d5969c36966a2610fdd2ca8dbae93764052717
ahmed-amr-12 <amra84747@gmail.com>
2026-03-04 18:25:22 +0200

    fix token from cookies in vercel

--- src/app/carts/UpdateCount.action.ts
+++ src/app/carts/UpdateCount.action.ts
@@ -1,24 +1,50 @@
+// "use server"
+// import { GetMyToken } from "@/Actions/GetMyToken";
+// import axios from "axios";
+
+// export async function UpdateCount(productId: string, count: number) {
+
+
+// const token =  await GetMyToken()
+
+// const { data } = await axios.put(`https://ecommerce.routemisr.com/api/v1/cart/${productId}`, {
+
+// count 
+// }, {
+//   headers: {
+//     token: token as string
+//   },
+
+
+// })
+
+
+// return data
+
+// }
+
+
 "use server"
 import { GetMyToken } from "@/Actions/GetMyToken";
 import axios from "axios";
 
 export async function UpdateCount(productId: string, count: number) {
 
+const token = await GetMyToken()
 
-const token =  await GetMyToken()
+if (!token) {
+  throw new Error("No token found")
+}
 
-const { data } = await axios.put(`https://ecommerce.routemisr.com/api/v1/cart/${productId}`, {
-
-count 
-}, {
-  headers: {
-    token: token as string
-  },
-
-
-})
-
+const { data } = await axios.put(
+  `https://ecommerce.routemisr.com/api/v1/cart/${productId}`,
+  { count },
+  {
+    headers: {
+      token: String(token)
+    }
+  }
+)
 
 return data
-
 }
