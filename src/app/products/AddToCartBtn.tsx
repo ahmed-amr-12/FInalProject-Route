e3db6049b00178d0cf27b63c23e713c8f3c0ff50
ahmed-amr-12 <amra84747@gmail.com>
2026-03-09 17:49:10 +0200

    toast login

--- src/app/products/AddToCartBtn.tsx
+++ src/app/products/AddToCartBtn.tsx
@@ -5,12 +5,23 @@ import { Button } from "@/components/ui/button";
 import React, { useContext } from "react";
 import { toast } from "sonner";
 import { CartContext } from "../_context/CartContext";
+import { useSession } from "next-auth/react";
 
 export default function AddToCartBtn({ productId }: { productId: string }) {
-  const { cartData , setCartData, numOfCartItems, setNumOfCartItems } =
+  const { cartData, setCartData, numOfCartItems, setNumOfCartItems } =
     useContext(CartContext);
 
+  const { status } = useSession();
+
   async function HandleAddItem() {
+
+    if (status !== "authenticated") {
+      toast.error("Please login first", {
+        position: "top-center",
+      });
+      return;
+    }
+
     const data = await AddItemToCarts(productId);
 
     if (data.status == "success") {
@@ -28,10 +39,10 @@ export default function AddToCartBtn({ productId }: { productId: string }) {
     <>
       <Button
         onClick={HandleAddItem}
-        className=" text-white  bg-slate-800"
+        className="text-white bg-slate-800"
         variant="outline"
       >
-        <i className="fa-solid fa-plus bg"></i> Add To Cart
+        <i className="fa-solid fa-plus"></i> Add To Cart
       </Button>
     </>
   );
